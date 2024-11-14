922c83a7fac2515a9724cfa1e1b3d97baea36852
Lukman_28 <lukmaneka216@gmail.com>
2024-11-14 22:58:02 +0700

    feat: add stream file
    W13: Jawaban Soal 2

---
+++ stream_lukman/lib/stream.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ColorStream {
+  final List<Color> colors = [
+    Colors.blueGrey,
+    Colors.amber,
+    Colors.deepPurple,
+    Colors.lightBlue,
+    Colors.teal,
+    Colors.pinkAccent,
+    Colors.blueAccent,
+    Colors.greenAccent,
+    Colors.redAccent,
+    Colors.orangeAccent,
+  ];
+}
